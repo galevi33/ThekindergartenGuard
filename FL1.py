08518f3f82faf9eb7a26a73d3ac9b5d80f8f4faa
Galevi <33134187+galevi33@users.noreply.github.com>
2019-01-17 15:19:22 +0200

    b

--- FL1.py
+++ FL1.py
@@ -1,2 +1,7 @@
-def inroom1():
- return "r1"
+from flask import Flask, jsonify, render_template, request
+import datetime
+def checkin():
+   aaa=datetime.datetime.now()
+   f = open("G_"+request.form["garden"]+" C_"+request.form["class"]+"_checkinDB.txt","a") 
+   f.write(request.form["kid"]+"_"+request.form["direction"]+"_"+aaa.strftime("%m-%d-%Y.%H:%M:%S")+"\n") 
+   f.close() 
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 from flask import Flask, jsonify, render_template, request
+from FL1 import checkin
+
+
 app = Flask(__name__)
-from FL1 import inroom1
-
-
  
-@app.route('/signUp',methods = ['POST'])
-def signUp():
-    f = open("testfile.txt","w") 
-    f.write("Hello World") 
-    f.close() 
-    return inroom1()+" "+request.form["hh"]
+# @app.route('/signUp',methods = ['POST'])
+# def signUp():
+#     f = open("testfile.txt","w") 
+#     f.write("Hello World") 
+#     f.close() 
+#     return " f "+request.form["hh"]
    
-  
+   
+@app.route('/checkin',methods = ['POST'])
+def Mcheckin():
+    checkin()
+    return "checkin-good"
+   
 
 if __name__ == "__main__":
    app.run()
